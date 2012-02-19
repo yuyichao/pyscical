00239f377141551ca02617affc576d92a0b1f18f
Yichao Yu <yyc1992@gmail.com>
2012-02-19 18:05:10 -0500

    add units

--- module/pyscical/units.py
+++ module/pyscical/units.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Copyright 2011 Yu Yichao
 # yyc1992@gmail.com
 #
@@ -18,10 +16,51 @@
 # You should have received a copy of the GNU General Public License
 # along with PySciCal.  If not, see <http://www.gnu.org/licenses/>.
 
-from math import *
-from numpy import *
-from pylab import *
-from .phyconsts import *
-from .astro import *
-from . import atom_mass as at_m
-from . import units
+from .phyconsts import c, eV
+
+km = 1e3
+dm = 1e-1
+cm = 1e-2
+mm = 1e-3
+um = 1e-6
+nm = 1e-9
+pm = 1e-12
+fm = 1e-15
+
+ft = foot = 0.3048
+inch = 25.4 * mm
+yd = yard = 3 * ft
+mile = 5280 * ft
+
+L = liter = 1e-3
+dL = 1e-4
+cL = 1e-5
+mL = 1e-6
+
+# Common time units
+
+ms = 1e-3
+us = 1e-6
+ns = 1e-9
+ps = 1e-12
+
+minute = 60
+hour = 3600
+day = 24 * hour
+
+a = year = 365 * day
+
+g = 1e-3
+mg = 1e-6
+ug = 1e-9
+
+kPa = 1e3
+bar = 1e5
+pound = 0.45359237
+psi = pound / inch ** 2
+
+dHg0 = 13.5951
+mmHg = dHg0 * 9.80665
+quart = 231 * inch**3
+
+ly = lightyear = c * year
