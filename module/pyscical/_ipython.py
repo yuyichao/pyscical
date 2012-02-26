e006419e98439c2546d18b624b97febdbd736c81
Yichao Yu <yyc1992@gmail.com>
2012-02-26 18:53:27 -0500

    nb_save

--- module/pyscical/_ipython.py
+++ module/pyscical/_ipython.py
@@ -18,15 +18,23 @@
 # You should have received a copy of the GNU General Public License
 # along with PySciCal.  If not, see <http://www.gnu.org/licenses/>.
 
-try:
-    get_ipython
-except NameError:
-    pass
-else:
-    from ._ipython import *
+# IPython Specifit stuff
 
-from ._general import *
-from .phyconsts import *
-from .astro import *
-from . import atom_mass as at_m
-from . import units
+from IPython.utils import py3compat
+import os
+
+ipython = get_ipython()
+
+def nb_save(fname):
+    hist_gen = ipython.shell.history_manager.get_range()
+    if not fname.endswith('.py'):
+        fname += '.py'
+    if os.path.isfile(fname):
+        ans = input('File `%s` exists. Overwrite (y/[N])? ' % fname)
+        if ans.lower() not in ['y','yes']:
+            print('Operation cancelled.')
+            return
+    with open(fname, 'w', encoding='utf-8') as f:
+        f.write("# coding: utf-8\n")
+        for hist in hist_gen:
+            f.write(py3compat.cast_unicode(hist[2]) + '\n')
