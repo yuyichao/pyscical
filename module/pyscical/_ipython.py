8805875c702d855d4cdd1645df7789b3f25fb259
Yichao Yu <yyc1992@gmail.com>
2014-05-04 08:36:32 -0400

    [_ipython.nb_save] update

--- module/pyscical/_ipython.py
+++ module/pyscical/_ipython.py
@@ -24,7 +24,7 @@ import os
 ipython = get_ipython()
 
 def nb_save(fname):
-    hist_gen = ipython.shell.history_manager.get_range()
+    hist_gen = ipython.history_manager.get_range()
     if not fname.endswith('.py'):
         fname += '.py'
     if os.path.isfile(fname):
@@ -33,6 +33,7 @@ def nb_save(fname):
             print('Operation cancelled.')
             return
     with open(fname, 'w', encoding='utf-8') as f:
+        f.write("#!/usr/bin/env python\n")
         f.write("# coding: utf-8\n")
         for hist in hist_gen:
             f.write(py3compat.cast_unicode(hist[2]) + '\n')
