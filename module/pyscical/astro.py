39ba7e087cf4bc0659b189ff51f5feb2b730eaf9
Yichao Yu <yyc1992@gmail.com>
2014-05-04 08:17:27 -0400

    [pyscical] license

--- module/pyscical/astro.py
+++ module/pyscical/astro.py
@@ -1,20 +1,20 @@
-# Copyright 2012 Yu Yichao
+#!/usr/bin/env python
+#
+# Copyright (C) 2012~2014 by Yichao Yu
 # yyc1992@gmail.com
 #
-# This file is part of PySciCal.
-#
-# PySciCal is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# PySciCal is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with PySciCal.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 M_sun = 1.9891e30
 R_sun = 6.955e8
